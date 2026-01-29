50f36c558fcbac964b237b993a57dc8e209cef9b
mahdi_ald <mahdiald.2003@gmail.com>
2026-01-29 14:41:00 +0100

    opgave 1-10

--- src/FirstTorsdag.java
+++ src/FirstTorsdag.java
@@ -6,6 +6,7 @@ void main(){
     double subtotal = a + b + c;
     double taxRate = 25;
     double withTax = (subtotal * taxRate/100);
+    System.out.println("Opgave 1");
     System.out.println(subtotal);
     System.out.println(subtotal+withTax);
 
@@ -16,6 +17,7 @@ void main(){
     int pep = 4;
     double total = food+drink+tip;
     double prPerson = total/pep;
+    System.out.println("opgave 2");
     System.out.println(prPerson);
 
 //opgave 3
@@ -24,7 +26,76 @@ void main(){
     double off = OGprice-(OGprice*disc/100);
     double tax = 25;
     double FinalPrice = off+(off * tax/100);
+    System.out.println("Opgave 3");
     System.out.println(OGprice);
     System.out.println(off);
     System.out.println(FinalPrice);
+
+//opgave 4
+    int guest = 15;
+    int cupcake = 40;
+    int CCprG = cupcake/guest;
+    int leftovers = cupcake % guest;
+    System.out.println("opgave 4");
+    System.out.println("Each guest gets " + CCprG + " cupcakes");
+    System.out.println(leftovers + " cupcakes left");
+
+//opgave 5
+    StringBuilder sb = new StringBuilder();
+            String name = "Anna";
+            int age = 25;
+            sb.append("Hello, ");
+            sb.append(name);
+            sb.append("! ");
+            sb.append("You are ");
+            sb.append(age);
+            sb.append(" years old");
+    System.out.println("opgave 5");
+    System.out.println(sb.toString());
+
+//opgave 6
+    System.out.println("opgave 6");
+    int score = 100;
+    System.out.println(score);
+    score += 50;
+    System.out.println(score);
+    score += 25;
+    System.out.println(score);
+
+//opgave 7
+    System.out.println("opgave 7");
+    int health = 100;
+    System.out.println(health);
+    health -= 15;
+    System.out.println(health);
+    health -= 20;
+    System.out.println(health);
+
+//opgave 8
+    double price = 50;
+    System.out.println(price);
+    price *= 2;
+    System.out.println(price);
+
+//opgave 9
+    System.out.println("opgave 9");
+    int counter = 10;
+    System.out.println(counter);
+    counter ++;
+    System.out.println(counter);
+    counter --;
+    System.out.println(counter);
+
+//opgave 10
+    System.out.println("opgave 10");
+    double number = 10;
+    System.out.println(number);
+    number += 5;
+    System.out.println(number);
+    number -= 3;
+    System.out.println(number);
+    number ++;
+    System.out.println(number);
+    number --;
+    System.out.println(number);
 }
